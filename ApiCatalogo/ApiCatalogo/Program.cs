3ecafadd63c3d4259e6b822f43b091ffd756721d
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2022-12-13 22:19:19 -0300

    Testando JWT

--- ApiCatalogo/ApiCatalogo/Program.cs
+++ ApiCatalogo/ApiCatalogo/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 internal class Program
@@ -16,7 +17,35 @@ internal class Program
         // Add services to the container.
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = @"JWT Authorization header using the Bearer scheme.
+                    Enter 'Bearer'[space].Example: \'Bearer 12345abcdef\'",
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+        });
 
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -70,9 +99,9 @@ internal class Program
         //definir os endpoints depois de dar o build.
 
         app.MapGet("/categorias", async (AppDbContext db) =>
-        await db.Categorias.ToListAsync()).RequireAuthorization();
+        await db.Categorias.ToListAsync()).WithTags("Categoria").RequireAuthorization();
 
-        app.MapGet("/categorias{id:int}", async(int id, AppDbContext db)=> {
+        app.MapGet("/categorias/{id:int}", async(int id, AppDbContext db)=> {
             
             return await db.Categorias.FindAsync(id)
             is Categoria categoria
@@ -120,9 +149,9 @@ internal class Program
         });
         //---------------------------Endpoints Produto-----------------------
         app.MapGet("/produtos", async (AppDbContext db) => 
-        await db.Produtos.ToListAsync()).RequireAuthorization();
+        await db.Produtos.ToListAsync()).WithTags("Produtos").RequireAuthorization();
 
-        app.MapGet("/produtos{id:int}", async (int id, AppDbContext db) => {
+        app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) => {
 
             return await db.Produtos.FindAsync(id)
             is Produto produto
