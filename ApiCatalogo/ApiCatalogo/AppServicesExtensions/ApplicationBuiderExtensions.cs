3e198c3ff08a362de6ae37d3936eae2c943a3113
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2022-12-14 11:09:32 -0300

    Organizando Program

---
+++ ApiCatalogo/ApiCatalogo/AppServicesExtensions/ApplicationBuiderExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace ApiCatalogo.AppServicesExtensions
+{
+    public static class ApplicationBuiderExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app,
+            IWebHostEnvironment environment)
+        {
+            if(environment.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            return app;
+        }
+
+        public static IApplicationBuilder UseAppCors(this IApplicationBuilder app)
+        {
+            app.UseCors(p => {
+                p.AllowAnyOrigin(); // politica que vai receber requisição de qualquer origem
+                p.WithMethods("GET");// somente get
+                p.AllowAnyHeader(); // com qualqualquer cabeçalho.
+            });
+            return app;
+        }
+
+        public static IApplicationBuilder UseSwaggerMiddleware(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { });
+            return app;
+        }
+    }
+}
